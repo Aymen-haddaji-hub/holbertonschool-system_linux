9887ace6c4a91b12fcb98fe0f22a12d8bf491c39
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-04-14 21:19:50 +0100

    fix betty style

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -1,11 +1,11 @@
 #ifndef _HLS_H_
 #define _HLS_H_
 
-//Used for basic input/output stream
+/*Used for basic input/output stream*/
 #include <stdio.h>
-//Used for handling directory files
+/*Used for handling directory files*/
 #include <dirent.h>
-//For EXIT codes and error handling
+/*For EXIT codes and error handling*/
 #include <errno.h>
 #include <stdlib.h>
 #endif /* _HLS_H_ */
