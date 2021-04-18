c6cb4a72f1d4685cdd0b1ede9defba1a14c96a2e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-04-18 20:58:29 +0100

    parsing lines and args

---
+++ 0x00-ls/handle_args.c
@@ -0,0 +1,47 @@
+#include "hls.h"
+/**
+ * handle_args - get ls options
+ * @argc: argument count
+ * @argv: arguments
+ * @op: table of options
+ */
+list_t *handle_args(int argc, char const *argv[], int *op)
+{
+	int i = 1, j = 1;
+	list_t *file_names = NULL;
+
+	while (i < argc)
+	{
+		if (argv[i][0] == '-' && _strlen(argv[i]) > 1)
+		{
+			j = 1;
+			while (_strlen(argv[i]) > j)
+			{
+				switch (argv[i][j])
+				{
+				case '1':
+					op[0] = 1;
+					break;
+				case 'l':
+					op[1] = 1;
+					break;
+				case 'a':
+					op[2] = 1;
+					break;
+				case 'A':
+					op[3] = 1;
+					break;
+				default:
+					break;
+				}
+				j++;
+			}
+		}
+		else
+		{
+		   file_names = add_node(&file_names, argv[i]);
+		}
+		i++;
+	}
+	return (file_names);
+}
