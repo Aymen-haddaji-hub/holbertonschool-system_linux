0c9a266cecaf194f3a5602a9162097b91aab7c02
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-06-10 23:49:41 +0100

    add comments

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -4,24 +4,24 @@ global asm_strlen
 
 section .text
 asm_strlen:
-    push rbp; setup the pointer to memory
-    mov rbp, rsp; 
+	push rbp; push the pointer to memory
+	mov rbp, rsp; 
 
-    mov rcx, 0h ; set rcx to NULL               
+	mov rcx, 0h ; set rcx to NULL               
 
-    
+	
 
 
 loop_asm_strlen:
 
 
-    cmp byte[rdi + rcx], 0h ; check the length of the str 
+	cmp byte[rdi + rcx], 0h ;  
 	je end; if null end loop
-    inc rcx; if no increment
-    jmp loop_asm_strlen; and then reloop
+	inc rcx; increment
+	jmp loop_asm_strlen; and then reloop
 end:
-    mov rax, rcx; put the rcx to return register.
-    mov rsp, rbp; 
-	pop rbp
+	mov rax, rcx; put the rcx to return register.
+	mov rsp, rbp; move the last pointer to the fist
+	pop rbp; 
 
-    ret
+	ret
