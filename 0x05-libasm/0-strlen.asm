662181bb1f6aa006ed1d2a3afc8db050d4af48ea
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-06-14 22:01:43 +0100

    fix betty style

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -4,20 +4,20 @@ global asm_strlen
 
 section .text
 asm_strlen:
-	push rbp; push the pointer to memory
-	mov rbp, rsp;
+	push rbp	;push the pointer to memory
+	mov rbp, rsp
 
-	mov rcx, 0h ; set rcx to NULL
+	mov rcx, 0h	;set rcx to NULL
 loop_asm_strlen:
 
 
 	cmp byte[rdi + rcx], 0h ;
-	je end; if null end loop
-	inc rcx; increment
-	jmp loop_asm_strlen; and then reloop
+	je end	;if null end loop
+	inc rcx	;increment
+	jmp loop_asm_strlen	;and then reloop
 end:
-	mov rax, rcx; put the rcx to return register.
-	mov rsp, rbp; move the last pointer to the fist
-	pop rbp;
+	mov rax, rcx	;put the rcx to return register.
+	mov rsp, rbp	;move the last pointer to the fist
+	pop rbp
 
 	ret
