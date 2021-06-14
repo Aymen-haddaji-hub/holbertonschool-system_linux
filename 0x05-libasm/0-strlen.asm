77bd5248fd17bae595e42f8bb60fb774170c315a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-06-14 22:07:34 +0100

    fix betty style 2

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -16,7 +16,7 @@ loop_asm_strlen:
 	inc rcx								;increment
 	jmp loop_asm_strlen					;and then reloop
 end:
-	mov rax, rcx						;put the rcx to return register.
+	mov rax, rcx						;put the rcx to return register
 	mov rsp, rbp						;move the last pointer to the fist
 	pop rbp
 
