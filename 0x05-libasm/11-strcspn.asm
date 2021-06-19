8c5c5fd0911e87f8ff4755f2288ccf0fc3a1c9ef
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-06-19 15:24:27 +0100

    betty

--- 0x05-libasm/11-strcspn.asm
+++ 0x05-libasm/11-strcspn.asm
@@ -1,4 +1,3 @@
- 
 BITS 64
 
 global asm_strcspn
@@ -12,28 +11,27 @@ asm_strcspn:
 
 loop:
 	cmp	byte[rdi + rcx], 0 ; Compare str[i] == '\0'
-	je	end_strcspn	   ; if previous is true jump to end
-	jmp	check_args	   ; Jump to check args str2
+	je	end_strcspn	       ; if previous is true jump to end
+	jmp	check_args	       ; Jump to check args str2
 
 check_args:
 	mov	r8b,	[rdi + rcx]
 	mov	r9b,	[rsi + r15]
-	cmp	r8b,  	r9b	; Compare str[i] and str2[j];
-	je	end_strcspn	; if previous is true jump to end
-	cmp	r9b	, 0 ; if rsi[j] == '\0' jump
-	je	reset_count	   ; reset counter
+	cmp	r8b,  	r9b             ; Compare str[i] and str2[j];
+	je	end_strcspn             ; if previous is true jump to end
+	cmp	r9b	, 0                 ; if rsi[j] == '\0' jump
+	je	reset_count	            ; reset counter
 	inc 	r15
-	jmp	check_args	; Back to previous function
+	jmp	check_args	            ; Back to previous function
 
 reset_count:
 	xor	r15,  r15
-	inc	rcx		   ; i++
-	jmp  	loop		; back to loop
+	inc	rcx		                ; i++
+	jmp  	loop		        ; back to loop
 
 end_strcspn:
 
 	mov 	rax,	rcx
 	mov	    rsp,	rbp
 	pop	    rbp
-    
 	ret
