eb5423c72e02036347b8832836055801ec91d92a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-06-20 00:05:34 +0100

    task 2

---
+++ 0x04-readelf/2-hreadelf.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - read elf file
+ * @argc: arguments conter
+ * @argv: argument vector
+ * @env:  environment
+ * Return: -1 in fail and 0 in success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	(void)argc;
+	args[3] = argv[1];
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
