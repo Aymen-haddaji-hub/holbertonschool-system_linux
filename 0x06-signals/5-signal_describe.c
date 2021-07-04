f395fe6b85cbe90d321177b259fd029b9f78326f
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-07-04 21:27:14 +0100

    5-signal_describe.c

---
+++ 0x06-signals/5-signal_describe.c
@@ -0,0 +1,20 @@
+#include "signals.h"
+
+#define USAGE "Usage: %s <signum>\n"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Success or Failure
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (printf(USAGE, argv[0]), EXIT_FAILURE);
+	if (strsignal(atoi(argv[1])))
+		printf("%d: %s\n", atoi(argv[1]), strsignal(atoi(argv[1])));
+	else
+		printf("%d Unknown signal %d\n", atoi(argv[1]), atoi(argv[1]));
+	return (EXIT_SUCCESS);
+}
