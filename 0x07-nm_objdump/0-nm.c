371ba8d985fc91d2c5a23f662e5dbe51dcd87938
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-08-27 15:15:29 +0100

    fix output

--- 0x07-nm_objdump/0-nm.c
+++ 0x07-nm_objdump/0-nm.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char **argv, char **env)
 {
-	char *args[] = {"/usr/bin/nm", "-p", "", NULL};
+	char *args[] = {"./hnm", "-p", "", NULL};
 
 	(void)argc;
 	args[2] = argv[1];
